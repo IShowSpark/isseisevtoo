2e51b37f0dc4a8b8c8f593835de0ac38cba552ec
opilane <opilane@HP-CZC6297BB7>
2022-02-23 12:48:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isseisevtoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int[] nums1 = { 5, 4, 3, 2, 1 };
+            int[] nums2 = { 2, 5, 4, 1, 3 };
+            bool n = false;
+            bool n1 = false;
+            bool n3 = false;
+            n3 = Correct(nums2);
+            n1 = Correct(nums1);
+            n = Correct(nums);
+
+            Console.WriteLine("Массив {0} упорядочен по возрастанию.", (n == true) ? "" : "не");
+            Console.WriteLine("Массив {0} упорядочен по возрастанию.", (n1 == true) ? "" : "не");
+            Console.WriteLine("Массив {0} упорядочен.", (n3 == true) ? "" : "не");
+            Console.Read();
+        }
+        static bool Correct(int[] data)
+        {
+            if (data.Length < 2)
+                return false;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (i == data.Length - 1)
+                    if (data[data.Length - 1] > data[data.Length - 2])
+                        return true;
+                    else
+                        return false;
+                if (data[i] < data[i + 1])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isseisevtoo
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            var n = int.Parse(Console.ReadLine());
+            var a = new int[n];
+            var r = new Random();
+            var count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                a[i] = r.Next(-2000, 2000);
+                if (a[i]>-10 && a[i] <10) count++;
+            }
+            Console.WriteLine("Count {0}", count);
+            Console.ReadKey();
+
+        }
+    }
+}
